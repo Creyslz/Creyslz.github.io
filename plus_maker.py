5932ccaedb33ee35e68a4d278496b0e3703f8abb
Charles Shang <creyslz@gmail.com>
2018-07-30 05:36:01 -0500

    qwrteqrt

---
+++ plus_maker.py
@@ -0,0 +1,12 @@
+cx = 708
+cy = 35
+r = 1.5
+
+print("" + str(cx) + " " + str(cy) + ","
+           + str(cx + r) + " " + str(cy) + ","
+           + str(cx) + " " + str(cy) + ","
+           + str(cx) + " " + str(cy + r) + ","
+           + str(cx) + " " + str(cy) + ","
+           + str(cx - r) + " " + str(cy) + ","
+           + str(cx) + " " + str(cy) + ","
+           + str(cx) + " " + str(cy - r))
